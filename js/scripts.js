14fc2ee0ae472f5b40e44edb28658168fbebfb2a
Xudoberdi <xudoberdimuhammadjonov571@gmail.com>
2022-12-02 00:04:23 +0500

    John Portfolio

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.querySelector(".header__togle--btn");
+const nav = document.querySelector("nav");
+
+toggleBtn.addEventListener("click", () => {
+  sitenav.classList.toggle("hidden");
+});
